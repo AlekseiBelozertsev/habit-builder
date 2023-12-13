1ff49a20355844ee9c3f62a48afffe4bc418b1c3
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-12-13 13:50:26 +0100

    feat: button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export type ButtonType = "rectangle" | "round";
+
+type ButtonProps = {
+  type: ButtonType;
+};
+
+const Button = () => {
+  return <button></button>;
+};
+
+export default Button;
