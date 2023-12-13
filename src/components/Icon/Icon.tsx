c1e673049ce22add8600c63ad2c40ba80ef1ae5c
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-12-13 15:50:18 +0100

    fix: icon component updated

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 type IconProps = {
-  path: string;
+  path: string | undefined;
   w: number;
   h: number;
 };
 
-const Icon: React.FC<IconProps> = ({ path, w, h }) => {
-  return <img src={path} width={w} height={h} alt={`Icon`} />;
+const Icon: React.FC<IconProps> = ({ path, w }) => {
+  const remToPx = (rem: number) => rem * 4 - 2;
+  const iconSize = remToPx(w);
+  return <img src={path} width={iconSize} height={iconSize} alt={`Icon`} />;
 };
 
 export default Icon;
