6cdaf3bb782055fa01c68169b3e8023ba3c9bdb1
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-12-13 15:52:39 +0100

    feat: header collection

---
+++ src/collections/Header/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Button from "../../components/Button";
+const menuIcon = require("../../assets/menu.svg").default;
+const searchIcon = require("../../assets/search.svg").default;
+
+const Header: React.FC = () => {
+  return (
+    <header className='flex w-screen container flex-row justify-between items-center p-2 bg-slate-50'>
+      <div className='flex flex-col'>
+        <h1 className="font-['Roboto'] font-bold">{"Habits to build"}</h1>
+        <span className="font-['Roboto'] text-sm font-normal">
+          {"Choose from available"}
+        </span>
+      </div>
+      <div className='flex flex-row gap-2'>
+        <Button
+          height={6}
+          width={6}
+          iconPath={searchIcon}
+          background={``}
+          onClick={() => alert("Hahaha, you dead.")}
+          type={`round`}
+        />
+        <Button
+          height={6}
+          width={6}
+          iconPath={menuIcon}
+          background={`bg-slate-200`}
+          onClick={() => alert("Hahaha, you dead and you shit yourself.")}
+          type={`round`}
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
