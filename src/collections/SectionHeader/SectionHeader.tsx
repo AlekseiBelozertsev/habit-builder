6d61f5d42fac6cf2b525261f3cda5bbbf51d9bd5
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-12-14 11:06:58 +0100

    feat: section header collection

---
+++ src/collections/SectionHeader/SectionHeader.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button } from "../../components";
+
+type SectionHeaderProps = {
+  title: string;
+};
+
+const SectionHeader: React.FC<SectionHeaderProps> = ({ title }) => {
+  return (
+    <div className='flex flex-row justify-between items-center px-4 h-16'>
+      <h2 className={`font-['Roboto'] font-bold`}>{title}</h2>
+      <Button
+        height={9}
+        width={6}
+        textColor={`text-color`}
+        background={`bg-slate-300`}
+        onClick={() =>
+          alert(
+            "Hahaha, you died and you shit yourself, peed and you died in front of your crash.",
+          )
+        }
+        type={`rectangle`}
+        text={`Show all`}
+      />
+    </div>
+  );
+};
+
+export default SectionHeader;
