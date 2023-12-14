68ff2bb3012ce0ba38ca6b81462b2c1bc09425ba
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-12-14 11:05:54 +0100

    feat: collection barrel file

---
+++ src/collections/index.ts
@@ -0,0 +1,5 @@
+import Header from "./Header/Header";
+import Habit from "./HabitElement/Habit";
+import SectionHeader from "./SectionHeader/SectionHeader";
+
+export { Header, Habit, SectionHeader };
