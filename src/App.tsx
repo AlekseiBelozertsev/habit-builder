778d13ddce7393432d54d09cf048e6da74504bd0
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-12-15 11:17:25 +0100

    feat: router implementation on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import Header from "./collections/Header/Header";
+import { Outlet } from "react-router";
+import { Habit, Header, SectionHeader } from "./collections";
+import Footer from "./collections/Footer/Footer";
 
 function App() {
   return (
-    <div className='flex w-screen h-full container bg-gray-700'>
+    <div className='flex flex-col justify-between w-screen h-screen container bg-gray-200'>
       <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
