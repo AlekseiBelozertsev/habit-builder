83e76fcc334ed50a74738249d88b7b6db0aadba9
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-12-19 15:42:40 +0100

    fix: navigation implemented

--- src/collections/Footer/Footer.tsx
+++ src/collections/Footer/Footer.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router";
 import { Button } from "../../components";
 import { navigation } from "../../config";
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <footer className='flex container w-full flex-row justify-between items-center mt-1 rounded-t px-4 h-16 py-2 gap-2 bg-white'>
       {navigation.map((navItem, i) => {
@@ -11,7 +13,7 @@ const Footer = () => {
             background={`bg-slate-200`}
             height={7}
             width={7}
-            onClick={navItem.onClick}
+            onClick={() => navigate(`${navItem.path}`)}
             type={`round`}
             iconPath={navItem.iconSrc}
             key={i}
