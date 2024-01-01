5f605a64c8af95b535add7e8c797f3205eef766a
Alexei Belozertsev <belozertsev.alexei@gmail.com>
2024-01-01 17:57:43 +0100

    fix: browser router - will not use API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { Outlet } from "react-router";
+import { Outlet, Route, Routes } from "react-router";
 import { Habit, Header, SectionHeader } from "./collections";
 import Footer from "./collections/Footer/Footer";
+import { Dashboard, HabitPage, Home, MyHabits, PageNotFound } from "./pages";
+import { BrowserRouter } from "react-router-dom";
+import { allHabits } from "./config";
 
 function App() {
+  const data = allHabits;
   return (
-    <div className='flex flex-col justify-between h-screen items-center bg-gray-200'>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />}>
+          <Route index element={<Home />} />
+          <Route path='my-habits' element={<MyHabits />} />
+          <Route path='my-habits/:id' element={<HabitPage data={data} />} />
+          <Route path='/:id' element={<HabitPage data={data} />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
