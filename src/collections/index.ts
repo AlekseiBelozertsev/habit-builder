026fe9dd1da99814ff364fca487afc4d6a36e336
Alexei Belozertsev <belozertsev.alexei@gmail.com>
2024-01-01 17:59:34 +0100

    feat: new filed added to the barrel files

--- src/collections/index.ts
+++ src/collections/index.ts
@@ -3,5 +3,6 @@ import Habit from "./HabitElement/Habit";
 import SectionHeader from "./SectionHeader/SectionHeader";
 import ScrollableCollection from "./ScrollableCollection/ScrollableCollection";
 import List from "./List/List";
+import Footer from "./Footer/Footer";
 
-export { Header, Habit, SectionHeader, ScrollableCollection, List };
+export { Header, Habit, SectionHeader, ScrollableCollection, List, Footer };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Home from "./HomePage/Home";
 import MyHabits from "./MyHabits/MyHabits";
 import HabitPage from "./HabitPage/HabitPage";
+import Dashboard from "./Dashboard/Dashboard";
+import PageNotFound from "./PageNotFound/PageNotFound";
 
-export { Home, MyHabits, HabitPage };
+export { Home, MyHabits, HabitPage, Dashboard, PageNotFound};
