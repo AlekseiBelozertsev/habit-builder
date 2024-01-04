9ddac33191f100378e76c8af714311fde5acc520
alexeibelozertsev <belozertsev.alexei@gmail.com>
2024-01-04 16:21:38 +0100

    feat: habit page update

--- src/pages/HabitPage/HabitPage.tsx
+++ src/pages/HabitPage/HabitPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Habit } from "../../config";
 import PageNotFound from "../PageNotFound/PageNotFound";
+import { Icon } from "../../components";
+import { List } from "../../collections";
 
 type HabitPageProps = {
   data: Habit[];
@@ -11,7 +13,19 @@ const HabitPage: React.FC<HabitPageProps> = ({ data }) => {
   const params = useParams();
   const currentPage = data.find((habit) => habit.id === params.id);
   if (currentPage) {
-    return <div>{currentPage?.id}</div>;
+    return (
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center p-7 rounded-full bg-white'>
+          <Icon h={18} w={18} path={currentPage?.illustration} />
+        </div>
+        <h3 className={`font-['Roboto, font-sans'] font-bold text-center`}>
+          {currentPage?.title}
+        </h3>
+        <span className={`font-['Roboto, font-sans'] text-gray-600 text-sm`}>
+          {currentPage?.subtitle}
+        </span>
+      </div>
+    );
   } else {
     return <PageNotFound />;
   }
